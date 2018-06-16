19c74d293809ddb487479135112446ae57ec1a3a
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-16 20:07:10 +0900

    lesson9

---
+++ lesson9.py
@@ -0,0 +1,11 @@
+# printで出力
+print('Hi')
+
+print('Hi', 'Mike')
+
+print('Hi', 'Mike', sep=',')
+
+print('Hi', 'Mike', sep=',', end='.\n')
+
+print('Hi', 'Mike', end='')
+print('Hi', 'Mike', end='')
