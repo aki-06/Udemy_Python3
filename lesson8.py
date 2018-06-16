57faa41c9d77d449b77de32bcc81afc302a655c1
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-16 19:57:46 +0900

    lesson8

---
+++ lesson8.py
@@ -0,0 +1,12 @@
+# 変数宣言
+num = 1
+name = 'Mike'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# 型変換
+new_num = str(num)
+print(new_num, type(new_num))
