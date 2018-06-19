807aafad04ca4dbc91565b16c332718228f58099
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-19 22:34:59 +0900

    lesson38

---
+++ lesson38.py
@@ -0,0 +1,16 @@
+# while, continue, break
+
+count = 0
+
+# while count < 5:
+#     print(count)
+#     count += 1
+
+while True:
+    if count >= 5:
+        break
+    if count == 2:
+        count += 1
+        continue
+    print(count)
+    count += 1
