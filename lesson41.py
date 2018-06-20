9be6101a88cd72eb06ae27c557aca08ab93a6ec0
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-20 18:10:38 +0900

    lesson41

---
+++ lesson41.py
@@ -0,0 +1,19 @@
+# for文、continue文、break文
+
+some_list = [1, 2, 3, 4, 5]
+
+# i = 0
+# while i < len(some_list):
+#     print(some_list[i])
+#     i += 1
+
+# for i in some_list:
+#     print(i)
+
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        continue
+    elif word == 'Mike':
+        break
+    print(word)
