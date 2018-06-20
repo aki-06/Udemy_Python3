f458e8a9e7ea4c092e2d08aeb91f9c17cef0b03b
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-20 22:44:01 +0900

    lesson47

---
+++ lesson47.py
@@ -0,0 +1,20 @@
+# 関数定義
+# def say_something():
+#     s = 'Hi'
+#     return s
+#
+# result = say_something()
+# print(result)
+#
+#
+def what_is_this(color):
+    if color == 'red':
+        print('tomato')
+    elif color == 'green':
+        print('green pepper')
+    else:
+        print("I don't know")
+
+what_is_this('red')
+what_is_this('green')
+what_is_this('yellow')
