872cdd282dbbcf6d6dab6e3c88a7d892a7cf76f6
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-20 21:40:12 +0900

    lesson42

---
+++ lesson42.py
@@ -0,0 +1,16 @@
+# for else文
+
+# for fruit in ['apple', 'banana', 'orange']:
+#     print(fruit)
+# else:
+#     print('I ate all!!')
+
+
+for i in [1, 2, 3, 4, 5]:
+    if i == 4:
+        print('aaa')
+        break
+    print(i)
+else:
+    print('complete!!')
+
