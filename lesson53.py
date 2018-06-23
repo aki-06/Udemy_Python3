8fc38fbcd40a48223cd66638ac392a73e00085b4
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-23 16:03:05 +0900

    lesson53

---
+++ lesson53.py
@@ -0,0 +1,18 @@
+# Docstring
+
+def sample_func(param1, param2):
+    """Example function with types documented in the docstring.
+
+    Args:
+        param1(int): The first parameter
+        param2(str: The second parameter
+
+    Returns:
+        bool: The return value. True for success. False otherwise.
+    """
+    print(param1)
+    print(param2)
+    return True
+
+# 関数のdocstringの確認
+print(sample_func.__doc__)
