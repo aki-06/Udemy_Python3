adde4d1606404b37847641adf8a85fa4a67d8c02
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-25 23:31:59 +0900

    lesson69

--- lesson67.py
+++ lesson67.py
@@ -4,9 +4,15 @@
 # from lesson_package import utils as u
 # 階層が増えたら.でつなぐ
 from lesson_package.talk import human
-
+from lesson_package.talk import animal
+# *を用いたimportも避ける
+# __init__.pyに記述したファイルを読み込める
+# from lesson_package.talk import *
 # r = utils.say_twice('Hello')
 # print(r)
 
-print(human.sing())
-print(human.cry())
+print(animal.sing())
+print(animal.cry())
+
+# print(human.sing())
+# print(human.cry())
---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,6 @@
+from lesson_package.tools import utils
+def sing():
+    return 'djfksdahfknsing'
+
+def cry():
+    return utils.say_twice('fkdnvuhaisufcry')
