a80fac3171a32e7142b2398e8f2bcde0fd0586ea
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-06-25 23:25:44 +0900

    lesson68

--- lesson67.py
+++ lesson67.py
@@ -1,8 +1,12 @@
 # import文とas
 # import lesson_package.utils
-from lesson_package import utils
 # asはあまり多用しすぎない方が良い
 # from lesson_package import utils as u
+# 階層が増えたら.でつなぐ
+from lesson_package.talk import human
 
-r = utils.say_twice('Hello')
-print(r)
+# r = utils.say_twice('Hello')
+# print(r)
+
+print(human.sing())
+print(human.cry())
