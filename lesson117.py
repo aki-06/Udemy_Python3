832962488b74547c0cb2a10435d937b625bb0e96
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-07-02 15:15:44 +0900

    lesson117

---
+++ lesson117.py
@@ -0,0 +1,42 @@
+# configparser
+
+"""
+[DEFAULT]
+debug = False
+
+[web_server]
+host = 127.0.0.1
+port = 80
+
+[db_server]
+host = 127.0.0.1
+port = 3360
+"""
+
+import configparser
+
+# config = configparser.ConfigParser()
+# config['DEFAULT'] = {
+#     'debug': True
+# }
+# config['web_server'] = {
+#     'host': '127.0.0.1',
+#     'port': 80
+# }
+# config['db_server'] = {
+#     'host': '127.0.0.1',
+#     'port': 3360
+# }
+
+# configファイルの書き込み
+# with open('config.ini', 'w') as config_file:
+#     config.write(config_file)
+
+# configファイルの読み取り
+config = configparser.ConfigParser()
+config.read('config.ini')
+print(config['web_server'])
+print(config['web_server']['host'])
+print(config['web_server']['port'])
+
+
