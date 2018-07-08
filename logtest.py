a12edbe5c4bc85c1f0aea0aa05bdf756fb667212
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-07-08 14:08:42 +0900

    lesson122

--- logtest.py
+++ logtest.py
@@ -3,6 +3,10 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# loggerを使ったものはlogtest.logに書き込まれる
+h = logging.FileHandler('logtest.log')
+logger.addHandler(h)
+
 def do_something():
     logger.info('from logtest')
     logger.debug('from logtest debug')
