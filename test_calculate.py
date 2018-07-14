c22d88241899c02a5475e2a707e251d6423d2b62
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-07-14 13:48:23 +0900

    lesson166

--- test_calculate.py
+++ test_calculate.py
@@ -3,8 +3,10 @@
 """
 https://docs.pytest.org/en/latest/
 """
+import pytest
 
 import calculation
+
 
 # def test_add_num_and_double():
 #     cal = calculation.Cal()
@@ -15,3 +17,8 @@ class TestCal(object):
     def test_add_num_and_double(self):
         cal = calculation.Cal()
         assert cal.add_num_and_double(1, 1) == 4
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = calculation.Cal()
+            assert cal.add_num_and_double('1', '1')
