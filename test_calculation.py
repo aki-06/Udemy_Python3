79a850ca5f9b84e39db3d9cda7788af385c5e146
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-07-14 10:55:17 +0900

    lesson162

--- test_calculation.py
+++ test_calculation.py
@@ -7,9 +7,19 @@ import calculation
 
 class CalTest(unittest.TestCase):
     def test_add_num_and_double(self):
+        """
+        正しく計算がされているか確認するテスト
+        """
         cal = calculation.Cal()
         self.assertEqual(cal.add_num_and_double(1, 1), 4)
-        self.assertEqual(cal.add_num_and_double(2, 2), 8)
+
+    def test_add_num_and_double_raise(self):
+        """
+        ValueErrorが発生するか確認するためのテスト
+        """
+        cal = calculation.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double('1', '1')
 
 
 if __name__ == '__main__':
