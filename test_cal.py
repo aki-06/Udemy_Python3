4be0c582acf0f42f77814c7d0483f339c429a6e1
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-07-15 12:33:46 +0900

    lesson172

--- test_cal.py
+++ test_cal.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import shutil
 import calc
 
 class TestCal(object):
@@ -7,7 +8,17 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = calc.Cal()
+        cls.test_dir = '/tmp/test_dir'
         cls.test_file_name = 'test1.txt'
+
+    def teardown_class(cls):
+        if os.path.exists(cls.test_dir):
+            shutil.rmtree(cls.test_dir)
+
+    def test_save_no_dir(self):
+        self.cal.save(self.test_dir, self.test_file_name)
+        test_file_path = os.path.join(self.test_dir, self.test_file_name)
+        assert os.path.exists(test_file_path) is True
 
     def test_add_num_and_double(self, csv_file):
         print(csv_file)
@@ -17,5 +28,9 @@ class TestCal(object):
         self.cal.save(tmpdir, self.test_file_name)
         test_file_path = os.path.join(tmpdir, self.test_file_name)
         assert os.path.exists(test_file_path) is True
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double('1', '1')
 
 
